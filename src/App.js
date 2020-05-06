a0f89aedf717087db94673a811289bf62e56af6c
Eric <ericbellet@hotmail.com>
2020-05-06 21:28:26 +0200

    Testing react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          La bebs es bella.
+        </p>
+        <a
+          className="App-link"
+          href="https://datablackgold.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
